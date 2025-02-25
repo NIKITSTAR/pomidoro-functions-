242a2dabaaeea5011c575fdb6270f5f0e2cffcd6
Nikita Starodubtsev <nik.workwork@gmail.com>
2025-02-25 17:34:23 +0700

    Задание 4. Циклы (использованы for и while)

--- tests/number_sum.py
+++ tests/number_sum.py
@@ -1,7 +1,19 @@
 def number_sum():
     n = int(input("Введите число: "))
-    spisok = list(range(1, n + 1))  # Создаём список [1, 2, ..., n]
-    summa = sum(spisok)            # Суммируем все элементы списка
+
+    # Формируем список чисел [1, 2, ..., n] с помощью цикла while
+    i = 1
+    spisok = []
+    while i <= n:
+        spisok.append(i)
+        i += 1
+
+    # Суммируем числа в spisok с помощью цикла for
+    summa = 0
+    for num in spisok:
+        summa += num
+
+    # Выводим результаты
     print("Числа:", *spisok)
     print(f"Сумма чисел: {summa}")
 
